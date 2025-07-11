e6dd9d3fb107592626a37f30ebbc7f3eaa7677b1
Clément Levaillant <clement.levaillant@mobsuccess.com>
2025-07-11 09:54:45 +0200

    add vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("affiche le lien Learn React", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
