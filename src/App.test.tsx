8dd8accfa871c61d8c09c959663c0758a685c294
Clément Levaillant <clement.levaillant@mobsuccess.com>
2025-07-17 08:49:13 +0200

    Migrates to Vitest for testing
    Replaces React Scripts test runner with Vitest.
    
    This change involves:
    - Removing unnecessary dependencies.
    - Adding Vitest and its related dependencies.
    - Updating the test script in package.json.
    - Modifying the setupTests.ts file to be compatible with Vitest.
    - Rewriting the App.test.tsx file to use Vitest's testing framework.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import "@testing-library/jest-dom";
+
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App", () => {
+  it("affiche le lien Learn React", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
